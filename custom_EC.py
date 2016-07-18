0feee96f356afa45d61ee17ac6574e5e42344e0c
Stephen Walker <swalker@feepay.com>
2016-07-18 14:03:52 -0500

    updated descriptions for common_EC

--- custom_EC.py
+++ custom_EC.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.expected_conditions import (_find_element, _find
 class exact_text_to_be_present_in_element(object):
     """ An expectation for checking if the given text is present in the
     specified element. Must be an exact match.
-    Custom function for FeePay
+    Custom function for TIES
     locator, text
     """
     def __init__(self, locator, text_):
@@ -25,7 +25,7 @@ class exact_text_to_be_present_in_element(object):
 class child_element_to_be_visible_in_element(object):
     """ An expectation for checking if the given element is visible in the
     parent element.
-    Custom function for FeePay
+    Custom function for TIES
     locator, parent_element
     """
     def __init__(self, locator, parent_element):
@@ -42,7 +42,7 @@ class child_element_to_be_visible_in_element(object):
 class element_text_to_be_non_null(object):
     """ An expectation for checking if any text is present in the
     specified element.
-    Custom function for FeePay
+    Custom function for TIES
     locator
     """
     def __init__(self, locator):
