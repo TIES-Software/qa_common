c48ac1df124e3ceab749374d272c5b1cc3318187
Mfoor <michael.foor@ties.k12.mn.us>
2016-08-25 11:17:09 -0500

    added firefox code to inta

--- globaldata.py
+++ globaldata.py
@@ -4,6 +4,7 @@ import platform, os
 REPORT_DIR = os.getcwd() + "/reports/"
 SCREENSHOT_DIR = os.getcwd() + "/reports/screenshots/"
 CHROME_DRIVER_DIR = os.environ["CHROME_DRIVER_DIR"]
+FIREFOX_BINARY_DIR = os.environ["FIREFOX_BINARY_DIR"]
 IE_DRIVER_DIR = "/Users/Briel/qa/common/IEDriverServer.exe"
 SELENIUM = "selenium"
 
