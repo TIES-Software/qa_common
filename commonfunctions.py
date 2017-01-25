220e94ef46ca0ed83ae7bd3a8bd10b688dbd55f2
Stephen Walker <swalker@feepay.com>
2017-01-25 15:06:58 -0600

    added verbose option, fixed iframe commonfunction

--- commonfunctions.py
+++ commonfunctions.py
@@ -263,7 +263,7 @@ def wait_for_element_not_visible(self, by, locator, timeout=globaldata.TIMEOUTSH
 # needed for adminBeta testing
 def wait_for_element_visible_in_iframe(self, iframe_id, by, locator, timeout=globaldata.TIMEOUTSHORT):
     driver = self.driver
-    assert wait_for_element_visible(self, globaldata.ID, iframe_id, globaldata.TIMEOUTSHORT), "iframe not present in page"
+    assert wait_for_element_visible(self, globaldata.ID, iframe_id, timeout), "iframe '" + iframe_id + "' not present in page"
     driver.switch_to.frame(iframe_id)
     try:
         visible = wait_for_element_visible(self, by, locator, timeout)
