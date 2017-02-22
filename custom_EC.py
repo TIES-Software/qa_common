1f2219bba63d5b23d9d513df913b5801bf288541
Stephen Walker <swalker@feepay.com>
2017-02-22 14:59:26 -0600

    fixed individual intervention tests

--- custom_EC.py
+++ custom_EC.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.expected_conditions import (_find_element, _find_elements, _element_if_visible)
 
+
 class exact_text_to_be_present_in_element(object):
     """ An expectation for checking if the given text is present in the
     specified element. Must be an exact match.
@@ -16,11 +17,12 @@ class exact_text_to_be_present_in_element(object):
         self.text = text_
 
     def __call__(self, driver):
-        try :
+        try:
             element_text = _find_element(driver, self.locator).text
             return self.text == element_text
         except StaleElementReferenceException:
             return False
+
 
 class child_element_to_be_visible_in_element(object):
     """ An expectation for checking if the given element is visible in the
@@ -33,11 +35,30 @@ class child_element_to_be_visible_in_element(object):
         self.parent = parent_element
 
     def __call__(self, driver):
-        try :
+        try:
             child_element = self.parent.find_element(*self.locator)
             return _element_if_visible(child_element)
         except StaleElementReferenceException:
             return False
+
+
+class child_element_to_be_present_in_element(object):
+    """ An expectation for checking if the given element is present in the
+    parent element.
+    Custom function for TIES
+    locator, parent_element
+    """
+    def __init__(self, locator, parent_element):
+        self.locator = locator
+        self.parent = parent_element
+
+    def __call__(self, driver):
+        try:
+            child_element = self.parent.find_element(*self.locator)
+            return child_element if child_element.is_enabled() else False
+        except StaleElementReferenceException:
+            return False
+
 
 class element_text_to_be_non_null(object):
     """ An expectation for checking if any text is present in the
@@ -49,7 +70,7 @@ class element_text_to_be_non_null(object):
         self.locator = locator
 
     def __call__(self, driver):
-        try :
+        try:
             element = _find_element(driver, self.locator)
             if element.text is not "":
                 return element
